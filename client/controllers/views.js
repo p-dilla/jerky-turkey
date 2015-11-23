b838e68a87a343229973cf7b99adf9e823d4a511
p-dilla <patrice.dillard@synechron.com>
2015-11-23 17:10:04 -0500

    add temp version

--- client/controllers/views.js
+++ client/controllers/views.js
@@ -1,19 +1,10 @@
 angular.module('myApp').controller('dashController',
   ['$scope', '$http', 'AppService',
   function ($scope, $http, AppService) {
-      debugger;
-      activate();
-
-      function activate() {
-        AppService.getData().then(function(response){
-          $scope.currentUser = response;
-          getByUsername(response.username);
-        });
-
-      }
-      function getByUsername(username) {
-        AppService.getByUsername(username).then(function(response){
-          $scope.userLinks = response;
-        });
-      }
+      AppService
+        .getData()
+        .then(function () {
+            $scope.currentUser = AppService.data;
+            debugger;
+        })
 }]);
--- client/services/appServices.js
+++ client/services/appServices.js
@@ -1,36 +1,12 @@
-angular.module('myApp').factory('AppService', function($http, $q) {
-    var appServiceFactory = {};
+angular.module('myApp').factory('AppService', function($http) {
+    var vm = this;
+    var data = [];
 
-  // @_getData
-  var _getData = function () {
-      var deferred = $q.defer();
-      
-      $http.get('/user/getCurrent').success(function (response) {
-
-          deferred.resolve(response);
-
-      }).error(function (err, status) {
-          deferred.reject(err);
-      });
-
-      return deferred.promise;
-  };
-
-  var _getByUsername = function (username) {
-      var deferred = $q.defer();
-      
-      $http.get('/link/findby/'+ username).success(function (response) {
-
-          deferred.resolve(response);
-
-      }).error(function (err, status) {
-          deferred.reject(err);
-      });
-
-      return deferred.promise;
-  };
-
-  appServiceFactory.getData = _getData;
-  appServiceFactory.getByUsername = _getByUsername;
-  return appServiceFactory;
+    this.getUser = function() {
+        return $http.get('/user/getCurrent')
+            .success(function (d) {
+                vm.data = d;
+                console.log(d);
+            });
+    };
 });
