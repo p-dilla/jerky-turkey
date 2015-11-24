afdd0a72ea3abcb49b01c2dbe5ab9ecfe20833eb
patrice <patrice.dillard@synechron.com>
2015-11-23 20:49:52 -0500

    app service without q

--- client/controllers/views.js
+++ client/controllers/views.js
@@ -2,7 +2,7 @@ angular.module('myApp').controller('dashController',
   ['$scope', '$http', 'AppService',
   function ($scope, $http, AppService) {
       AppService
-        .getData()
+        .getUser()
         .then(function () {
             $scope.currentUser = AppService.data;
             debugger;
--- client/services/appServices.js
+++ client/services/appServices.js
@@ -1,4 +1,4 @@
-angular.module('myApp').factory('AppService', function($http) {
+angular.module('myApp').service('AppService', function($http) {
     var vm = this;
     var data = [];
 
